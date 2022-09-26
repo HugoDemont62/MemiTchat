fd5d9c77900bb27f08dcada74631461388560eee
HugoDemont <hugodemont62@hotmail.fr>
2022-09-26 08:46:45 +0200

    Commit MemiTchat 26 sep 2022

---
+++ app/src/main/java/com/example/memitchat/LoginRequest.java
@@ -0,0 +1,27 @@
+package com.example.memitchat;
+
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginRequest extends StringRequest {
+
+    private final Map<String,String> params;
+
+
+    public LoginRequest(MainActivity mainActivity, String user_email, String password,Response.Listener<String> listener) {
+
+        super(Method.POST, "", listener, null);
+        params = new HashMap<>();
+        params.put("useremail", user_email);
+        params.put("password", password);
+
+    }
+    @Override
+    public Map<String, String> getParams(){
+        return params;
+    }
+}
---
+++ app/src/main/java/com/example/memitchat/RegisterRequest.java
@@ -0,0 +1,27 @@
+package com.example.memitchat;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterRequest extends StringRequest {
+
+    private final Map<String,String> params;
+
+
+    public RegisterRequest(MainActivity mainActivity, String user_email, String user_name, String password,Response.Listener<String> listener) {
+
+        super(Method.POST, "", listener, null);
+        params = new HashMap<>();
+        params.put("useremail", user_email);
+        params.put("username", user_name);
+        params.put("password", password);
+
+    }
+    @Override
+    public Map<String, String> getParams(){
+        return params;
+    }
+}
